6f85bb21c0a330f6c25b09420357aa4bc89b0a69
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-01-24 16:32:13 +0100

    creation des methodes de Entite

--- src/fr/insarouen/asi/prog/asiaventure/elements/Entite.java
+++ src/fr/insarouen/asi/prog/asiaventure/elements/Entite.java
@@ -4,10 +4,45 @@ import fr.insarouen.asi.prog.asiaventure.Monde;
 
 public class Entite{
 	//Attributs
-	
+	String nom;
+	Monde monde;
 	//Constructeur
-	public Entite(java.lang.String nom, Monde monde){
+	public Entite(String nom, Monde monde){
+		this.nom=nom;
+		this.monde=monde;
+	}
+	//methodes
+	public String getNom(){
+		return this.nom;
+	}
+	
+	public Monde getMonde(){
+		return this.monde;
+	}
+	
+	public String toString(){
+		return String.format("Entite %s du monde %s",nom,monde.toString());
+	}
+	
+	public boolean equals(Object o){
+		if (o==this){
+			return true;
+		}
+		if (o==null || this.getClass() != o.getClass()){
+			return false;
+		}
+		Entite test = (Entite)o;
+		if(this.nom == test.nom && this.monde == test.monde){
+			return true;
+		}
+		return false;
+	}
+	
+	public int hashCode(){
+		return 2*nom.hashCode()+3*monde.hashCode() ;
+	}
+	//main
+	public static void main(String[] args){
 		
 	}
-	//methodes 
 }
