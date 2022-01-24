8b9a5fbaa4dcf8ae7d662b55ed56d8359e734824
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-01-24 16:06:12 +0100

    Création arbo + classe Entite et classe monde (vide)

---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/Entite.java
@@ -0,0 +1,13 @@
+package fr.insarouen.asi.prog.asiaventure.elements ;
+
+import fr.insarouen.asi.prog.asiaventure.Monde;
+
+public class Entite{
+	//Attributs
+	
+	//Constructeur
+	public Entite(java.lang.String nom, Monde monde){
+		
+	}
+	//methodes 
+}
