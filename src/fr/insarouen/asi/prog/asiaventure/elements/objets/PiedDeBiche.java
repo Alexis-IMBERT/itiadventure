cae071f856191b9cff54b691a2229376fec27d0d
Saissi Hassani Mehdi <mehdi.saissi_hassani@insa-rouen.fr>
2022-02-16 17:01:42 +0000

    Ajout de la javadoc de PiedDeBiche.java

--- src/fr/insarouen/asi/prog/asiaventure/elements/objets/PiedDeBiche.java
+++ src/fr/insarouen/asi/prog/asiaventure/elements/objets/PiedDeBiche.java
@@ -2,10 +2,30 @@ package fr.insarouen.asi.prog.asiaventure.elements.objets ;
 
 import fr.insarouen.asi.prog.asiaventure.Monde;
 
+/**
+ * Classe PiedDeBiche basé sur la classe abstraite Objet
+ * @see Objet
+ */
+
 public class PiedDeBiche extends Objet{
+	
 	//Attributs
+	
+	/**
+	 * Attribut de type Boolean indiquant que l'Objet est deplacable
+	 */
+
 	private boolean estDeplacable = true;
+	
 	//Constructeur
+	
+	/**
+	 * Constructeur d'un Pied de biche
+	 * @see Objet
+	 * @param nom Chaine de caractère désignant le pied de biche
+	 * @param monde Monde dans lequel se trouve le pied de biche
+	 */
+	
 	public PiedDeBiche(String nom, Monde monde){
 		super(nom,monde);
 	}
