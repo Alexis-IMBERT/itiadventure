f897aec60e0e39420c19d6bce510e8ca5f852fa9
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-21 16:18:05 +0100

    Ajout de la java doc de fr.insarouen.asi.prog.asiaventure.elements.structure.PieceException

--- src/fr/insarouen/asi/prog/asiaventure/elements/structure/PieceExeption.java
+++ src/fr/insarouen/asi/prog/asiaventure/elements/structure/PieceExeption.java
@@ -1,10 +1,20 @@
 package fr.insarouen.asi.prog.asiaventure.elements.structure;
 
+/**
+* @throws Exception liée aux pièces
+*/
 public class PieceException extends ElementStructurelException{
 	//Constructeur
+	/**
+	* Constructeur de l'exception de type PieceException qui hérite de ElementStructurelException
+	*/
 	public PieceException(){
 		super();
 	}
+	
+	/**
+	* Constructeur de l'exception de type PieceException avec une chaine de caractère (un message) donné en paramètre qui hérite de ElementStructurelException
+	*/
 	public PieceException(java.lang.String msg){
 		super(msg);
 	}
