87dd2d36123d9113bcfdd09209c86da347942ab2
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-21 17:21:44 +0100

    ajout du fichier VivantException

---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/vivants/VivantException.java
@@ -0,0 +1,10 @@
+package fr.insarouen.asi.prog.asiaventure.elements.vivants;
+
+public class VivantException extends ASIAventureException{
+	public VivantException(){
+		super();
+	}
+	public VivantException(){
+		super();
+	}
+}
