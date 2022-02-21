f66ac448bbfd4f2cf27c9cd90b1c0f88963acbc3
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-21 16:12:59 +0100

    ajout de la classe exception fr.insarouen.asi.prog.asiaventure.elements.structure.PieceException

---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/structure/PieceExeption.java
@@ -0,0 +1,11 @@
+package fr.insarouen.asi.prog.asiaventure.elements.structure;
+
+public class PieceException extends ElementStructurelException{
+	//Constructeur
+	public PieceException(){
+		super();
+	}
+	public PieceException(java.lang.String msg){
+		super(msg);
+	}
+}
