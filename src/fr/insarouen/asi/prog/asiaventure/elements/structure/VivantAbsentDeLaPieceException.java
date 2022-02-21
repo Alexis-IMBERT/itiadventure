697ee4b94f7ffc53ac653a15ba17b3c9320f9ae7
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-21 16:32:21 +0100

    ajout du fichier

---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/structure/VivantAbsentDeLaPieceException.java
@@ -0,0 +1,12 @@
+package fr.insarouen.asi.prog.asiaventure.elements.structure.VivantAbsentDeLaPieceException;
+
+public class VivantAbsentDeLaPieceException extends PieceException{
+	//Constructeur
+	puclic VivantAbsentDeLaPieceException(){
+		super();
+	}
+	
+	puclic VivantAbsentDeLaPieceException(String msg){
+		super(msg);
+	}
+}
