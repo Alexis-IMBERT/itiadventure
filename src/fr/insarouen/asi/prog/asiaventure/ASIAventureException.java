964fae9410648030364a0ff0b40d235e7275e3ea
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-21 16:08:40 +0100

    Ajout d'element pour la java doc dans le fichier ASIAventureException pour les constructeur

--- src/fr/insarouen/asi/prog/asiaventure/ASIAventureException.java
+++ src/fr/insarouen/asi/prog/asiaventure/ASIAventureException.java
@@ -5,9 +5,16 @@ package fr.insarouen.asi.prog.asiaventure;
 */
 public class ASIAventureException extends java.lang.Exception{
 	//Constructeur
+	/**
+	* Constructeur de l'exception de type ASIAventureException qui hérite de java.lang.Exception
+	*/
 	public ASIAventureException(){
 		super();
 	}
+	
+	/**
+	* Constructeur de l'exception de type ASIAventureException avec une chaine de caractère qui hérite de java.lang.Exception
+	*/
 	public ASIAventureException(java.lang.String msg){
 		super(msg);
 	}
