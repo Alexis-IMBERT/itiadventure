c6ebac442d91464d6b85f804188e3790a43d47b6
Saissi Hassani Mehdi <mehdi.saissi_hassani@insa-rouen.fr>
2022-02-21 15:35:55 +0000

    Add new file

---
+++ src/fr/insarouen/asi/prog/asiaventure/MondeException.java
@@ -0,0 +1,26 @@
+package fr.insarouen.asi.prog.asiaventure
+
+/**
+*@throws création du type d'exceptions de MondeException
+*/
+
+public class MondeException extends ASIAventureException{
+	
+	//Constructeurs
+	
+	/**
+	* Constructeur de l'exception de type MondeException qui hérite de ASIAventureException
+	*@param msg Chaine de caractère désignant MondeException
+	*/
+	
+	public MondeException(String msg){
+		super(msg);
+	}
+	
+	/**
+	* Constructeur de l'exception de type MondeException qui hérite de ASIAventureException
+	*/
+	public MondeException(){
+		super();
+	}
+}
