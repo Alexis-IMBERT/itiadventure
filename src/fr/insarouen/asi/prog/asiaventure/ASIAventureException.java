2930e3c31f325088d066c31c5797bf2f6ca99448
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-21 15:59:50 +0100

    Ajout de la classe ASIAventureException

---
+++ src/fr/insarouen/asi/prog/asiaventure/ASIAventureException.java
@@ -0,0 +1,14 @@
+package fr.insarouen.asi.prog.asiaventure;
+
+/**
+* @throws création du type d'exceptions de ASIAventure
+*/
+public class ASIAventureException extends java.lang.Exception{
+	//Constructeur
+	public ASIAventureException(){
+		super();
+	}
+	public ASIAventureException(java.lang.String msg){
+		super(msg);
+	}
+}
