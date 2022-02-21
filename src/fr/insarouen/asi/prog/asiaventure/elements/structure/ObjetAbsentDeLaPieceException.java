20a090ab35bf4d930ffbd0b9fbcbc9469a2e2237
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-21 16:27:46 +0100

    ajout de fr.insarouen.asi.prog.asiaventure.elements.structure.ObjetAbsentDeLaPieceException

---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/structure/ObjetAbsentDeLaPieceException.java
@@ -0,0 +1,10 @@
+package fr.insarouen.asi.prog.asiaventure.elements.structure;
+
+public class ObjetAbsentDeLaPieceException extends PieceException{
+	ObjetAbsentDeLaPieceException(){
+		super();
+	}
+	ObjetAbsentDeLaPieceException(String msg){
+		super(msg);
+	}
+}
