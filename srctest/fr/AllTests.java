8713485b6efa58783589a3c7d1b937747ca5d900
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-28 17:18:30 +0100

    tests

--- srctest/fr/AllTests.java
+++ srctest/fr/AllTests.java
@@ -1,10 +1,12 @@
-package fr.insarouen.asi.prog.asiaventure.elements;
+package fr;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import fr.insarouen.asi.prog.asiaventure.elements.AllTestsElements;
+
 @RunWith(Suite.class)
 @SuiteClasses({
-	TestEntite.class
+	AllTestsElements.class
 })
-public class AllTestsElements{}
+public class AllTests{}
