b8e2b38f2ab02699dd577d20e471114f77d33250
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-02-28 17:06:43 +0100

    création de la suite de test Entite

--- srctest/fr/insarouen/asi/prog/asiaventure/elements/AllTestsElements.java
+++ srctest/fr/insarouen/asi/prog/asiaventure/elements/AllTestsElements.java
@@ -2,3 +2,8 @@ import org.junnit.runner.RunWith;
 import org.junnit.runners.Suite;
 import org.junnit.runners.Suite.SuiteClasses;
 
+@RunWith(Suite.class)
+@SuiteClasses({
+	TestEntite.class
+})
+public class AllTestsElements(){}
