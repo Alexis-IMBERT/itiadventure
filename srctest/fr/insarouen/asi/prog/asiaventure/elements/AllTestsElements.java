a82ad46e42bc72fa21bb1368b80f9196e6410648
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-03-04 20:35:45 +0100

    test de pied de biche

--- srctest/fr/insarouen/asi/prog/asiaventure/elements/AllTestsElements.java
+++ srctest/fr/insarouen/asi/prog/asiaventure/elements/AllTestsElements.java
@@ -3,8 +3,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import fr.insarouen.asi.prog.asiaventure.elements.objets.TestPiedDeBiche;
+
 @RunWith(Suite.class)
 @SuiteClasses({
-	TestEntite.class
+	TestEntite.class,
+	TestPiedDeBiche.class
 })
 public class AllTestsElements{}
---
+++ srctest/fr/insarouen/asi/prog/asiaventure/elements/objets/TestPiedDeBiche.java
@@ -0,0 +1,44 @@
+package fr.insarouen.asi.prog.asiaventure.elements.objets;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.tools.ant.taskdefs.condition.IsTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import fr.insarouen.asi.prog.asiaventure.Monde;
+import fr.insarouen.asi.prog.asiaventure.NomDEntiteDejaUtiliseDansLeMondeException;
+
+public class TestPiedDeBiche {
+	static Monde mondeTest;
+	PiedDeBiche piedDeBicheTest;
+
+	@BeforeClass
+	public static void avantClasseTest(){
+		mondeTest = new Monde("mondeTest");
+	}
+	@Before
+	public void avantTest() throws NomDEntiteDejaUtiliseDansLeMondeException{
+		piedDeBicheTest = new PiedDeBiche("piedDeBicheTest", mondeTest);
+	}
+
+	@Test
+	public void testConstructeur(){
+		assertEquals(piedDeBicheTest,piedDeBicheTest);
+	}
+
+	@Test
+	public void testIsEstDeplacable(){
+		assertTrue(piedDeBicheTest.isEstDeplacable());
+	}
+
+	@Test
+	public void testSetEstDeplacable(){
+		assertTrue(piedDeBicheTest.isEstDeplacable());
+		piedDeBicheTest.setEstDeplacable(false);
+		assertFalse(piedDeBicheTest.isEstDeplacable());
+	}
+}
