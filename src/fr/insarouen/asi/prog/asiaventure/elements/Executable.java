d570c6d1be7957c42deee08ffccd3d3b9660b621
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-03-21 17:53:27 +0100

    code de l'interface executable
    Co-authored-by: Mehbrochh <Mehbrochh@users.noreply.github.com>

---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/Executable.java
@@ -0,0 +1,11 @@
+package fr.insarouen.asi.prog.asiaventure.elements;
+
+
+/**
+ * @author Alexis Imbert et Mehdi Saissi Hassani
+ * Interface de Executable
+
+ */
+public interface Executable{
+	public void executer() throws java.lang.Throwable;
+}
