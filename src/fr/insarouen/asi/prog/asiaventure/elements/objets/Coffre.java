e7454c2a26a0aaa7d887ae668f6ac8ae90a6358e
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-03-21 15:54:11 +0100

    début du TP07

---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/objets/Coffre.java
@@ -0,0 +1,7 @@
+// @u:start Coffre
+
+// Le coffre est un simple objet non déplaçable, c'est juste pour créer un nouveau type d'objet
+// il ne contient pas d'objet
+// il n'a pas de serrure
+
+// @u:end Coffre
