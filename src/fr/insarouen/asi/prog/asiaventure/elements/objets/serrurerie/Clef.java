157bed492b6d1fe6718759a7da9a1802fccf236a
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2022-03-21 17:53:52 +0100

    implémentation  de clé

--- src/fr/insarouen/asi/prog/asiaventure/elements/objets/serrurerie/Clef.java
+++ src/fr/insarouen/asi/prog/asiaventure/elements/objets/serrurerie/Clef.java
@@ -1,5 +1,18 @@
 package fr.insarouen.asi.prog.asiaventure.elements.objets.serrurerie;
 
-public class Clef {
-	
+import fr.insarouen.asi.prog.asiaventure.Monde;
+import fr.insarouen.asi.prog.asiaventure.NomDEntiteDejaUtiliseDansLeMondeException;
+import fr.insarouen.asi.prog.asiaventure.elements.objets.Objet;
+
+
+public class Clef extends Objet{
+    //Constructeurs
+    protected Clef(String nom,Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException{
+        super(nom,monde);
+    }
+	//Methodes
+    public boolean estDeplacable(){
+        return true;
+    }
+
 }
